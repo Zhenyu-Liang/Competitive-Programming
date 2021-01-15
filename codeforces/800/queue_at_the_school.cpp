52e85a7d2d2ae868f60bfb1303b25ef875dfa0bb
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-01-14 20:12:04 -0500

    Update queue_at_the_school.cpp

--- codeforces/800/queue_at_the_school.cpp
+++ codeforces/800/queue_at_the_school.cpp
@@ -5,15 +5,12 @@ using namespace std;
 
 int main()
 {
-    int n, t;
     string s;
-    cin >> n >> t >> s;
-    while (t--)
-    {
-        for (int i = 1; i < n; ++i)
-        {
-            if (s[i] == 'G' && s[i-1] == 'B')
-            {
+    int n, t;
+    cin >>n >> t >> s;
+    while(t--) {
+        for(int i=1;i<n;++i){
+            if (s[i]=='G'&&s[i-1]== 'B'){
                 s[i] = 'B';
                 s[i-1] = 'G';
                 ++i;

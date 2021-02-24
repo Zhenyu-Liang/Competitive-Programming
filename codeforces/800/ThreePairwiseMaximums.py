26d0521afdf5eb4e93506b9d3fd673fc01f3ffe3
Aaron <baokou66@gmail.com>
2021-02-24 12:03:49 -0500

    improved on a problem

--- codeforces/800/ThreePairwiseMaximums.py
+++ codeforces/800/ThreePairwiseMaximums.py
@@ -1,15 +1,9 @@
 for i in[*open(0)][1:]:
-    x=[*map(int,i.split())]
-    if (q:=len({*x}))==3 or x.count(min(x))>1 and q!=1:
+    a,b,c=sorted(map(int,i.split()))
+    if c>b:
         print("NO")
     else:
         print("YES")
-        if q==1:
-            print(*x)
-        else:
-            x=sorted(x)
-            x[-1]=1
-            print(*x) 
+        print(*[a,a,b])
         
-        
-        
+       
